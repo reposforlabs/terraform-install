3df0e078ca5ea5a6206a6b2bf98982880282a993
reposforlabs <160736679+reposforlabs@users.noreply.github.com>
2024-03-28 20:50:24 +0530

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -3,4 +3,4 @@ sudo apt-get remove needrestart -y
 sudo apt upgrade -y
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt install terraform
+sudo apt update && sudo apt install terraform -y
